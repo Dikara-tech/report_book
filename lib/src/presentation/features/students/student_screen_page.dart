bc5a99def246f1cb9bd8fcc4982bbbad35d7fe76
Muhammad Dafi <dafinoer@outlook.com>
2023-10-12 11:33:51 +0700

    create ui student list

--- lib/src/presentation/features/students/student_screen_page.dart
+++ lib/src/presentation/features/students/student_screen_page.dart
@@ -7,9 +7,31 @@ class StudentsScreenPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Students'),
+      ),
+      body: ListView.separated(
+        itemCount: 20,
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        separatorBuilder: (context, index) =>
+            const Padding(padding: EdgeInsets.symmetric(vertical: 4.0)),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Card(
+              child: ListTile(
+                title: Text('Title $index', style: theme.textTheme.titleMedium),
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
