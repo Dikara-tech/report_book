2698ee86c747171ec0685f9da5340b7866958f3a
Muhammad Dafi <dafinoer@outlook.com>
2023-10-12 11:18:33 +0700

    reate profile_screen_page.dart

---
+++ lib/src/presentation/features/profile/profile_screen_page.dart
@@ -0,0 +1,16 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class ProfileScreenPage extends StatelessWidget {
+  const ProfileScreenPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+    );
+  }
+}
