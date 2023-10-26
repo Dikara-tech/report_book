2d040b7bcbe6986b4249eb743d0b477378943979
Muhammad Dafi <dafinoer@outlook.com>
2023-10-26 10:13:24 +0700

    create chat contact

---
+++ lib/src/presentation/features/chat_contact/chat_contact_screen_page.dart
@@ -0,0 +1,30 @@
+import 'package:auto_route/annotations.dart';
+import 'package:dikara_core/dikara_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:report_book/src/presentation/features/chat_contact/bloc/contact_list/contact_list_cubit.dart';
+import 'package:report_book/src/presentation/routers/router.dart';
+import 'package:report_book_core/report_book_core.dart';
+
+part '_chat_list_contact.dart';
+
+part '_chat_card_widget.dart';
+
+@RoutePage()
+class ChatContactScreenPage extends StatelessWidget {
+  const ChatContactScreenPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search Contact'),
+      ),
+      body: MultiBlocProvider(providers: [
+        BlocProvider(
+          create: (context) => ContactListCubit.create()..getRelationUser(),
+        )
+      ], child: const _ChatListContact()),
+    );
+  }
+}
