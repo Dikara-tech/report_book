851f27426c48fc56aa8510b3f31ecb1edc15e233
Muhammad Dafi <dafinoer@outlook.com>
2023-10-26 23:32:40 +0700

    add more param

--- lib/src/presentation/features/create_student/create_student_screen_page.dart
+++ lib/src/presentation/features/create_student/create_student_screen_page.dart
@@ -1,7 +1,7 @@
 import 'package:dikara_core/dikara_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:report_book/src/presentation/features/create_student/bloc/register_student/register_student_cubit.dart';
+import 'package:report_book/src/presentation/features/create_student/bloc/input_student/input_student_cubit.dart';
 import 'package:report_book/src/presentation/features/create_student/provider/register_form/register_form_provider.dart';
 import 'package:report_book/src/widgets/custom_button_widget.dart';
 import 'package:report_book/src/widgets/custom_text_field_widget.dart';
@@ -15,7 +15,17 @@ part '_text_email_widget.dart';
 
 @RoutePage()
 class CreateStudentScreenPage extends StatelessWidget {
-  const CreateStudentScreenPage({super.key});
+  const CreateStudentScreenPage({
+    @QueryParam() this.isRegisterPage = true,
+    @QueryParam() this.userId,
+    @QueryParam() this.name,
+    @QueryParam() this.email,
+  });
+
+  final bool isRegisterPage;
+  final String? userId;
+  final String? email;
+  final String? name;
 
   @override
   Widget build(BuildContext context) {
@@ -25,23 +35,39 @@ class CreateStudentScreenPage extends StatelessWidget {
       ),
       body: MultiProvider(
         providers: [
-          ChangeNotifierProvider(create: (context) => RegisterFormProvider()),
-          BlocProvider(create: (context) => RegisterStudentCubit.create()),
+          ChangeNotifierProvider(
+            create: (context) => RegisterFormProvider(
+              name,
+              email,
+            ),
+          ),
+          BlocProvider(
+            create: (context) => InputStudentCubit.create(isRegisterPage),
+          ),
         ],
-        child: const _RegisterFormContentWidget(),
+        child: _RegisterFormContentWidget(
+          isRegister: isRegisterPage,
+          idUser: userId,
+        ),
       ),
     );
   }
 }
 
 class _RegisterFormContentWidget extends StatelessWidget {
-  const _RegisterFormContentWidget();
+  const _RegisterFormContentWidget({
+    this.isRegister = false,
+    this.idUser,
+  });
+
+  final bool isRegister;
+  final String? idUser;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return BlocListener<RegisterStudentCubit, ResourceState<void>>(
+    return BlocListener<InputStudentCubit, ResourceState<void>>(
       listener: (context, state) => state.maybeWhen(
         success: (code, message, data) => AutoRouter.of(context).pop(),
         error: (code, message, stacktrace, data) => SnackBar(
@@ -55,14 +81,17 @@ class _RegisterFormContentWidget extends StatelessWidget {
         ),
         orElse: () => null,
       ),
-      child: const SingleChildScrollView(
+      child: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            _TextEmailWidget(),
-            _TextNameWidget(),
-            _ButtonSubmitWidget(),
+            const _TextEmailWidget(),
+            const _TextNameWidget(),
+            _ButtonSubmitWidget(
+              isRegister: isRegister,
+              idUser: idUser,
+            ),
           ],
         ),
       ),
