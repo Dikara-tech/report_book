356b236268481b5569ac059e1a390a9b8cea09fd
Muhammad Dafi <dafinoer@outlook.com>
2023-10-26 22:28:47 +0700

    create text email widget

---
+++ lib/src/presentation/features/create_student/_text_email_widget.dart
@@ -0,0 +1,20 @@
+part of 'create_student_screen_page.dart';
+
+class _TextEmailWidget extends StatelessWidget {
+  const _TextEmailWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    final registerForm = context.watch<RegisterFormProvider>();
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: CustomTextFieldWidget(
+        hintText: 'Input Email Student',
+        textInputType: TextInputType.emailAddress,
+        errorText: registerForm.errorTextEmail,
+        onChange: (value) => context.read<RegisterFormProvider>().email = value,
+      ),
+    );
+  }
+}
