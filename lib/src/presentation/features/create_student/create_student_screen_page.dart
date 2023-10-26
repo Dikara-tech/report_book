afcfde795508f2a2e9feed56abee88aace592e10
Muhammad Dafi <dafinoer@outlook.com>
2023-10-26 22:30:49 +0700

    refactor bloc

--- lib/src/presentation/features/create_student/create_student_screen_page.dart
+++ lib/src/presentation/features/create_student/create_student_screen_page.dart
@@ -1,6 +1,7 @@
 import 'package:dikara_core/dikara_core.dart';
 import 'package:flutter/material.dart';
-import 'package:report_book/src/presentation/features/create_student/bloc/register_student/register_student_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:report_book/src/presentation/features/create_student/bloc/register_student/register_student_cubit.dart';
 import 'package:report_book/src/presentation/features/create_student/provider/register_form/register_form_provider.dart';
 import 'package:report_book/src/widgets/custom_button_widget.dart';
 import 'package:report_book/src/widgets/custom_text_field_widget.dart';
@@ -9,6 +10,8 @@ import 'package:report_book_core/report_book_core.dart';
 part '_button_submit_widget.dart';
 
 part '_text_name_widget.dart';
+
+part '_text_email_widget.dart';
 
 @RoutePage()
 class CreateStudentScreenPage extends StatelessWidget {
@@ -23,17 +26,44 @@ class CreateStudentScreenPage extends StatelessWidget {
       body: MultiProvider(
         providers: [
           ChangeNotifierProvider(create: (context) => RegisterFormProvider()),
-          Provider(create: (context) => RegisterStudentBloc.create())
+          BlocProvider(create: (context) => RegisterStudentCubit.create()),
         ],
-        child: const SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              _TextNameWidget(),
-              _ButtonSubmitWidget(),
-            ],
+        child: const _RegisterFormContentWidget(),
+      ),
+    );
+  }
+}
+
+class _RegisterFormContentWidget extends StatelessWidget {
+  const _RegisterFormContentWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return BlocListener<RegisterStudentCubit, ResourceState<void>>(
+      listener: (context, state) => state.maybeWhen(
+        success: (code, message, data) => AutoRouter.of(context).pop(),
+        error: (code, message, stacktrace, data) => SnackBar(
+          backgroundColor: Colors.black,
+          content: Text(
+            'Oops something wrong',
+            style: theme.textTheme.bodySmall?.copyWith(
+              color: Colors.white,
+            ),
           ),
+        ),
+        orElse: () => null,
+      ),
+      child: const SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            _TextEmailWidget(),
+            _TextNameWidget(),
+            _ButtonSubmitWidget(),
+          ],
         ),
       ),
     );
