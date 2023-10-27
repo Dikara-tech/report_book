4e18e773290546f7c9789fea5c357ff90d48a1dd
Muhammad Dafi <dafinoer@outlook.com>
2023-10-27 23:03:43 +0700

    delete file

--- lib/src/presentation/features/create_task/create_task_screen_page.dart
+++ lib/src/presentation/features/create_task/create_task_screen_page.dart
@@ -1,15 +1,19 @@
-import 'package:auto_route/auto_route.dart';
+import 'package:dikara_core/dikara_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:report_book/src/presentation/features/create_task/bloc/create_task/create_task_cubit.dart';
+import 'package:report_book/src/presentation/features/create_task/providers/form/task_form_provider.dart';
 import 'package:report_book/src/widgets/custom_button_widget.dart';
 import 'package:report_book/src/widgets/custom_text_field_widget.dart';
+import 'package:report_book_core/report_book_core.dart';
 
 part '_button_create_task_widget.dart';
 
 part '_textfield_title_widget.dart';
 
-part '_textfield_deadline_widget.dart';
-
 part '_textfield_detail_task_widget.dart';
+
+part '_radio_task_widget.dart';
 
 @RoutePage()
 class CreateTaskScreenPage extends StatelessWidget {
@@ -18,16 +22,46 @@ class CreateTaskScreenPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create Task'),
+      appBar: AppBar(title: const Text('Create Task')),
+      body: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => TaskFormProvider.create()),
+          BlocProvider(create: (context) => CreateTaskCubit.create())
+        ],
+        child: const _ContentCreateTask(),
       ),
-      body: const Column(
+    );
+  }
+}
+
+class _ContentCreateTask extends StatelessWidget {
+  const _ContentCreateTask();
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return BlocListener<CreateTaskCubit, ResourceState<void>>(
+      listener: (context, state) => state.maybeWhen(
+        orElse: () => null,
+        success: (code, message, data) => AutoRouter.of(context).pop(),
+        error: (code, message, stacktrace, data) => SnackBar(
+          backgroundColor: Colors.black,
+          content: Text(
+            'Failed create Task',
+            style: theme.textTheme.bodySmall?.copyWith(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+      child: const Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           _TextFieldTitleWidget(),
-          _TextFieldDeadlineWidget(),
           _TextFieldDetailTaskWidget(),
+          _RadioTaskWidget(),
           _ButtonCreateTaskWidget(),
         ],
       ),
