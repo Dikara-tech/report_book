d62cea4faca06cd09de2900cbfcda7738fc81289
Muhammad Dafi <dafinoer@outlook.com>
2023-10-27 14:14:50 +0700

    create announcement cubit

---
+++ lib/src/presentation/features/announcements/bloc/announcements/announcements_cubit.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:report_book/src/di/injection_container.dart';
+import 'package:report_book_core/report_book_core.dart';
+
+part 'announcements_state.dart';
+
+part 'announcements_cubit.freezed.dart';
+
+class AnnouncementsCubit extends Cubit<AnnouncementsState> {
+  final AnnouncementRepository _announcementRepository;
+  final UserRepository _userRepository;
+  StreamSubscription<List<AnnouncementModel>>? _streamSubscription;
+
+  AnnouncementsCubit(this._announcementRepository, this._userRepository)
+      : super(AnnouncementsState.defaultState());
+
+  factory AnnouncementsCubit.create() => AnnouncementsCubit(
+        inject.get(),
+        inject.get(),
+      );
+
+  Future<void> getAnnouncements() async {
+    try {
+      emit(state.copyWith(isLoading: true));
+      final classId = await _userRepository.getIdMemberClassUser();
+      if (classId != null) {
+        _onListen(classId);
+      } else {
+        emit(state.copyWith(isError: true));
+      }
+    } catch (error) {
+      emit(state.copyWith(isError: true));
+    }
+  }
+
+  void _onListen(String classId) {
+    _streamSubscription?.cancel();
+    _streamSubscription =
+        _announcementRepository.watchAnnouncement(classId).listen((event) {
+      emit(state.copyWith(isLoading: false, announcements: event));
+    });
+  }
+
+  @override
+  Future<void> close() {
+    _streamSubscription?.cancel();
+    return super.close();
+  }
+}
