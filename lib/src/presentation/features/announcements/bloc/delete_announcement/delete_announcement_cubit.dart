d299482ebc81903307e02f3b0c9ee5241736d6ac
Muhammad Dafi <dafinoer@outlook.com>
2023-10-27 14:15:39 +0700

    create delete_announcement_cubit.dart

---
+++ lib/src/presentation/features/announcements/bloc/delete_announcement/delete_announcement_cubit.dart
@@ -0,0 +1,5 @@
+import 'package:dikara_core/dikara_core.dart';
+
+class DeleteAnnouncementCubit extends Cubit<ResourceState<void>> {
+  DeleteAnnouncementCubit() : super(const ResourceState.initial());
+}
