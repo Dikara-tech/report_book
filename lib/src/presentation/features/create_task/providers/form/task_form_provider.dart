347d3105c0f23ccb21176d4bdc9bd4acdfc96220
Muhammad Dafi <dafinoer@outlook.com>
2023-10-27 23:02:24 +0700

    create task form provider

---
+++ lib/src/presentation/features/create_task/providers/form/task_form_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:report_book_core/report_book_core.dart';
+
+class TaskFormProvider extends ValueNotifier<TaskModel> {
+  TaskFormProvider(super.value);
+
+  factory TaskFormProvider.create() => TaskFormProvider(
+        const TaskModel(
+          taskTitle: '',
+          taskDetail: '',
+          createdAt: 0,
+          taskTypeModel: TaskTypeModel.DefaultTask,
+        ),
+      );
+
+  void onChangeTaskTitle(String title) {
+    value = value.copyWith(taskTitle: title);
+  }
+
+  void onChangeTaskDetail(String detailTask) {
+    value = value.copyWith(taskDetail: detailTask);
+  }
+
+  void onChangeTaskType(TaskTypeModel taskTypeModel) {
+    value = value.copyWith(taskTypeModel: taskTypeModel);
+  }
+}
