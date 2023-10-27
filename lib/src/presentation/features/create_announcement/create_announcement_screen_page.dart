a0fdadd5d164970a4e1e5c2a734f8adebc58a4d5
Muhammad Dafi <dafinoer@outlook.com>
2023-10-27 14:16:24 +0700

    change from stateless to stateful

--- lib/src/presentation/features/create_announcement/create_announcement_screen_page.dart
+++ lib/src/presentation/features/create_announcement/create_announcement_screen_page.dart
@@ -1,36 +1,97 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:dikara_core/dikara_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:report_book/src/presentation/features/create_announcement/bloc/create_announcement/crate_announcement_cubit.dart';
 import 'package:report_book/src/widgets/custom_button_widget.dart';
 import 'package:report_book/src/widgets/custom_text_field_widget.dart';
 
-part '_textfield_detail_announcement_widget.dart';
-
-part '_button_create_widget.dart';
-
-part '_textfield_title_widget.dart';
-
 @RoutePage()
-class CreateAnnouncementScreenPage extends StatelessWidget {
+class CreateAnnouncementScreenPage extends StatefulWidget {
   const CreateAnnouncementScreenPage({super.key});
 
   @override
+  State<CreateAnnouncementScreenPage> createState() =>
+      _CreateAnnouncementScreenPageState();
+}
+
+class _CreateAnnouncementScreenPageState
+    extends State<CreateAnnouncementScreenPage> {
+  late final TextEditingController _titleEditController;
+  late final TextEditingController _announcementEditController;
+  late final CrateAnnouncementCubit _crateAnnouncementCubit;
+
+  @override
+  void initState() {
+    super.initState();
+    _titleEditController = TextEditingController();
+    _announcementEditController = TextEditingController();
+    _crateAnnouncementCubit = CrateAnnouncementCubit.create();
+  }
+
+  @override
+  void dispose() {
+    _titleEditController.dispose();
+    _announcementEditController.dispose();
+    _crateAnnouncementCubit.close();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(title: const Text('Annoucement Detail')),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Expanded(
-            child: ListView(
-              children: const [
-                _TextFieldTitleWidget(),
-                _TextFieldDetailAnnouncementWidget()
-              ],
+      body: BlocListener<CrateAnnouncementCubit, ResourceState<void>>(
+        bloc: _crateAnnouncementCubit,
+        listener: (context, state) => state.when(
+          loading: (data) => null,
+          success: (code, message, data) => AutoRouter.of(context).pop(),
+          error: (code, message, stacktrace, data) => SnackBar(
+            backgroundColor: Colors.black,
+            content: Text(
+              'Failed create user',
+              style: theme.textTheme.bodySmall?.copyWith(
+                color: Colors.white,
+              ),
             ),
           ),
-          const _ButtonCreateWidget()
-        ],
+          initial: () => null,
+        ),
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: CustomTextFieldWidget(
+                textEditingController: _titleEditController,
+                hintText: 'Title Annoucement',
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: CustomTextFieldWidget(
+                textEditingController: _announcementEditController,
+                hintText: 'Detail Annoucement',
+                maxLines: 5,
+              ),
+            ),
+            SizedBox(
+              width: width,
+              child: CustomButtonWidget(
+                titleButton: 'Create Announcement',
+                onAction: () {
+                  _crateAnnouncementCubit.createAnnouncement(
+                    _titleEditController.text,
+                    _announcementEditController.text,
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
