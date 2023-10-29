1f36810ebd743e465792adad544602f1ba1aa05d
Muhammad Dafi <dafinoer@outlook.com>
2023-10-29 17:29:56 +0700

    create new page for home student

---
+++ lib/src/presentation/features/home/home_screen_student_page.dart
@@ -0,0 +1,59 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:report_book/src/presentation/routers/router.dart';
+
+@RoutePage()
+class HomeScreenStudentPage extends StatelessWidget {
+  const HomeScreenStudentPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return AutoTabsRouter(
+      homeIndex: 0,
+      routes: [
+        AnnouncementScreenRouter(isTeacher: false),
+        ScoresScreenRouter(isTeacher: false),
+        const ChatScreenRouter(),
+        TaskScreenRouter(isTeacher: false),
+        const ProfileScreenRouter()
+      ],
+      transitionBuilder: (context, child, animation) => FadeTransition(
+        opacity: animation,
+        child: child,
+      ),
+      builder: (context, child) => Scaffold(
+        body: child,
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (indexTab) =>
+              AutoTabsRouter.of(context).setActiveIndex(indexTab),
+          indicatorColor: theme.colorScheme.primary,
+          selectedIndex: AutoTabsRouter.of(context).activeIndex,
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.notifications_outlined),
+              label: 'Info',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.view_list),
+              label: 'Scores',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.message_outlined),
+              label: 'Message',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.calendar_month_outlined),
+              label: 'Tasks',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.person_outline),
+              label: 'Profile',
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/presentation/features/profile/bloc/profile/profile_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:dikara_core/dikara_core.dart';
+import 'package:report_book_core/report_book_core.dart';
+
+class ProfileCubit extends Cubit<ResourceState<UserModel>> {
+  final UserRepository userRepository;
+
+  ProfileCubit(this.userRepository) : super(const ResourceState.initial());
+
+  factory ProfileCubit.create() => ProfileCubit(inject.get());
+
+  Future<void> getProfile() async {
+    try {
+      emit(const ResourceLoading());
+      final profile = await userRepository.getUser();
+      if (profile != null) {
+        emit(ResourceSuccess(data: profile));
+      }
+    } catch (error) {
+      emit(const ResourceError());
+    }
+  }
+}
