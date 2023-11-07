f660f5a2f1549f341d766dd9cd0c200fabe499f1
Muhammad Dafi <dafinoer@outlook.com>
2023-11-07 15:00:26 +0700

    add percentage

--- lib/src/presentation/features/tasks/_fab_create_student_widget.dart
+++ lib/src/presentation/features/tasks/_fab_create_student_widget.dart
@@ -11,7 +11,7 @@ class _FabCreateStudentWidget extends StatelessWidget {
       return FloatingActionButton(
         heroTag: 'task',
         onPressed: () {
-          AutoRouter.of(context).push(CreateTaskScreenRouter(isEdit: true));
+          AutoRouter.of(context).push(CreateTaskScreenRouter(isEdit: false));
         },
         child: const Icon(Icons.add),
       );
